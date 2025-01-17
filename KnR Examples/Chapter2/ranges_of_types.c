1c053f12cddab50b9094c8f4d598ccf2328d4996
Athan <athanj2001@gmail.com>
2025-01-17 17:14:22 -0500

    begin Exercise 2-1

---
+++ KnR Examples/Chapter2/ranges_of_types.c
@@ -0,0 +1,8 @@
+/* Write a program to determine the ranves of char, short, int, and long variables, both signed and unsigned, by printing
+ * appropriate values from standard headers and by direct computeation. Harder if you compute them: determine the ranges 
+ * of the various floating-point types.
+ *
+ * Author: Athan Johnson
+ * Created: 1/17/2025
+ * Modified:
+ */
