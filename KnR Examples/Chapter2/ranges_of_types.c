ee6e9bc58e566e508956047080505388d7fe8bc8
Athan <athanj2001@gmail.com>
2025-01-19 16:48:30 -0500

    ranges_of_types now not only finishes before the heat death of the universe but is actually fast

--- KnR Examples/Chapter2/ranges_of_types.c
+++ KnR Examples/Chapter2/ranges_of_types.c
@@ -4,7 +4,7 @@
  *
  * Author: Athan Johnson
  * Created: 1/17/2025
- * Modified:
+ * Modified: 1/19/2025
  */
 
 #include <stdio.h>
@@ -22,7 +22,7 @@ int main ()
 	signed long s_long = 1;
 	unsigned long u_long = 1;
 
-	unsigned long iter = 0;
+	unsigned long iter = 1;
 
 	// here we cycle through each time until the current type loops back around
 	// in the case of signed we have iter be one behind and just print out the last positive value,
@@ -31,85 +31,84 @@ int main ()
 	// represent the max
 	while (s_char > 0)
 	{
-		s_char++;
-		iter++;
+		s_char = s_char*2;
+		iter = iter*2;
 	}
 
 
-	printf("Signed Char:\t\tMin:%d\t\tMax:%ld\n", s_char, iter);
+	printf("Signed Char:\t\tMin:%d\t\t\tMax:%ld\n", s_char, iter-1);
 
-	iter = 0; // reset iter every time
+	iter = 1; // reset iter every time
 
 	while (u_char > 0)
 	{
-		u_char++;
-		iter++;
+		u_char = u_char*2;
+		iter = iter*2;
 	}
 
-	printf("Unsigned Char:\t\tMin:%d\t\t\tMax:%ld\n", 0, iter);
+	printf("Unsigned Char:\t\tMin:%d\t\t\t\tMax:%ld\n", 0, iter-1);
 
-	iter = 0;
+	iter = 1;
 
 	while (s_short > 0)
 	{
-		s_short++;
-		iter++;
+		s_short = s_short*2;
+		iter = iter*2;
 	}
 
-	printf("Signed Short:\t\tMin:%d\t\tMax:%ld\n", s_char, iter);
+	printf("Signed Short:\t\tMin:%d\t\t\tMax:%ld\n", s_char, iter-1);
 
-	iter = 0;
+	iter = 1;
 
 	while (u_short > 0)
 	{
-		u_short++;
-		iter++;
+		u_short = u_short*2;
+		iter = iter*2;
 	}
 
-	printf("Unsigned Short:\t\tMin:%d\t\t\tMax:%ld\n", 0, iter);
+	printf("Unsigned Short:\t\tMin:%d\t\t\t\tMax:%ld\n", 0, iter-1);
 
-	iter = 0;
+	iter = 1;
 
 	while (s_int > 0)
 	{
-		s_int++;
-		iter++;
+		s_int = s_int*2;
+		iter = iter*2;
 	}
 
-	printf("Signed Int:\t\tMin:%d\t\tMax:%ld\n", s_int, iter);
+	printf("Signed Int:\t\tMin:%d\t\t\tMax:%ld\n", s_int, iter-1);
 
-	iter = 0;
+	iter = 1;
 
 	while (u_int > 0)
 	{
-		u_int++;
-		iter++;
+		u_int = u_int*2;
+		iter = iter*2;
 	}
 
-	printf("Unsigned Int:\t\tMin:%d\t\t\tMax:%ld\n", 0, iter);
+	printf("Unsigned Int:\t\tMin:%d\t\t\t\tMax:%ld\n", 0, iter-1);
 
-	iter = 0;
+	iter = 1;
 
 	while (s_long > 0)
 	{
-		s_long++;
-		iter++;
+		s_long = s_long*2;
+		iter = s_long;
 	}
 
-	printf("Signed Long:\t\tMin:%ld\t\tMax:%ld\n", s_long, iter);
+	printf("Signed Long:\t\tMin:%ld\tMax:%ld\n", s_long, iter-1);
 
-	iter = 0;
+	iter = 1;
+	u_long = 2; // this is because now we are at the limit of unsigned long and need to know ahead of time when it will go over
 
 	while (u_long > 0)
 	{
-		u_long++;
-		iter++;
+		u_long = u_long*2;
+		iter = iter*2;
 	}
+	iter = iter + (iter - 1); // it's essential that we add the exact right number to iter before it overflows because
+				  // otherwise it'll overflow back to zero
 
-	printf("Unsigned Long:\t\tMin:%d\t\t\tMax:%ld\n", 0, iter);
-
-	iter = 0;
-
-
+	printf("Unsigned Long:\t\tMin:%d\t\t\t\tMax:%lu\n", 0, iter);
 
 }
